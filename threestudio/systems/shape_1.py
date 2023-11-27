4044a43095a2170fc71c981087a4e125b41640f7
zzzzzsa <1727509672@qq.com>
2023-11-27 20:06:51 +0800

    11-27

--- threestudio/systems/shape_1.py
+++ threestudio/systems/shape_1.py
@@ -1,10 +1,13 @@
 from dataclasses import dataclass, field
 
 import torch
+import torch.nn.functional as F
+import csv
 
 import threestudio
 from threestudio.systems.base import BaseLift3DSystem
 from threestudio.utils.ops import binary_cross_entropy, dot
+from threestudio.utils.misc import cleanup, get_device
 from threestudio.utils.typing import *
 from threestudio.utils.imagesync import *
 
@@ -37,51 +40,56 @@ class DreamFusion(BaseLift3DSystem):
 
     def training_step(self, batch, batch_idx):
         out = self(batch)
+        
+        # 指定你的CSV文件名
+        filename = "batch.csv"
 
-        shape_img = sample_image(self.cfg.mesh_path,**batch)
+        # 写入CSV
+        with open(filename, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            # 可选：写入头部信息，例如键和值的标签
+            writer.writerow(["Key", "Value"])
+            # 写入字典键值对
+            for key, value in batch.items():
+                writer.writerow([key, value])
 
+        shape_img, projection_matrix = sample_image_4x4(self.cfg.mesh_path,**batch)
         #print(list(batch.keys()))
-        #print(batch.get('camera_positions'))
+        print(batch.get('mvp_mtx'))
+        print(projection_matrix)
 
         comp_rgb_img = out["comp_rgb"] * 255
         comp_rgb_img = comp_rgb_img.byte()
         #print(comp_rgb_img)
         comp_rgb_img = Image.fromarray(comp_rgb_img.cpu().squeeze(0).numpy())
         comp_rgb_img.save('comp_rgb_img.png')
-        shape_img.save('shape_sampled_img.png')
 
         prompt_utils = self.prompt_processor()
-        guidance_out = self.guidance(
-            out["comp_rgb"], prompt_utils, **batch, rgb_as_latents=False
-        )
+        # guidance_out = self.guidance(
+        #     out["comp_rgb"], prompt_utils, **batch, rgb_as_latents=False
+        # )
 
         loss = 0.0
 
+        rendered_img = out["comp_rgb"] * 255
+        rendered_img = rendered_img.squeeze(0)
+        rendered_img = rendered_img.permute(2,0,1)
+
+        if rendered_img.shape == shape_img.shape:
+            loss_l1 = F.l1_loss(rendered_img, shape_img)
+        else:
+            raise ValueError("Images must have the same dimensions.")
+        self.log("train/loss_l1", loss_l1)
+        loss += loss_l1
+
+        guidance_out = {
+            "loss_l1": loss_l1,
+        }
+        
         for name, value in guidance_out.items():
             self.log(f"train/{name}", value)
             if name.startswith("loss_"):
                 loss += value * self.C(self.cfg.loss[name.replace("loss_", "lambda_")])
-
-        if self.C(self.cfg.loss.lambda_orient) > 0:
-            if "normal" not in out:
-                raise ValueError(
-                    "Normal is required for orientation loss, no normal is found in the output."
-                )
-            loss_orient = (
-                out["weights"].detach()
-                * dot(out["normal"], out["t_dirs"]).clamp_min(0.0) ** 2
-            ).sum() / (out["opacity"] > 0).sum()
-            self.log("train/loss_orient", loss_orient)
-            loss += loss_orient * self.C(self.cfg.loss.lambda_orient)
-
-        loss_sparsity = (out["opacity"] ** 2 + 0.01).sqrt().mean()
-        self.log("train/loss_sparsity", loss_sparsity)
-        loss += loss_sparsity * self.C(self.cfg.loss.lambda_sparsity)
-
-        opacity_clamped = out["opacity"].clamp(1.0e-3, 1.0 - 1.0e-3)
-        loss_opaque = binary_cross_entropy(opacity_clamped, opacity_clamped)
-        self.log("train/loss_opaque", loss_opaque)
-        loss += loss_opaque * self.C(self.cfg.loss.lambda_opaque)
 
         for name, value in self.cfg.loss.items():
             self.log(f"train_params/{name}", self.C(value))
--- threestudio/utils/imagesync.py
+++ threestudio/utils/imagesync.py
@@ -1,4 +1,6 @@
 import torch
+import torchvision.transforms as transforms
+
 import numpy as np
 import trimesh
 import pyrender
@@ -7,7 +9,37 @@ import platform
 import os
 if platform.system() == "Linux":
     os.environ['PYOPENGL_PLATFORM'] = 'egl'
+import math
+
 from threestudio.utils.typing import *
+
+def get_camera_pose(position, azimuth, elevation):
+    # 确保输入是Tensor并且在GPU上
+    device = azimuth.device
+    azimuth_rad = azimuth * torch.pi / 180.0
+    elevation_rad = elevation * torch.pi / 180.0
+
+    # 计算旋转矩阵
+    R_azimuth = torch.tensor([
+        [torch.cos(azimuth_rad), -torch.sin(azimuth_rad), 0],
+        [torch.sin(azimuth_rad),  torch.cos(azimuth_rad), 0],
+        [0,                      0,                       1]
+    ], device=device)
+
+    R_elevation = torch.tensor([
+        [1, 0,                     0                     ],
+        [0, torch.cos(elevation_rad), -torch.sin(elevation_rad)],
+        [0, torch.sin(elevation_rad),  torch.cos(elevation_rad)]
+    ], device=device)
+
+    R = torch.mm(R_elevation, R_azimuth)
+
+    # 创建4x4变换矩阵
+    transform_matrix = torch.eye(4, device=device)
+    transform_matrix[:3, :3] = R
+    transform_matrix[:3, 3] = position
+
+    return transform_matrix
 
 def rotation_matrix_y(angle):
     device = angle.device
@@ -26,6 +58,33 @@ def rotation_matrix_x(angle):
         [torch.tensor(0.0, device=device), c, -s],
         [torch.tensor(0.0, device=device), s, c]
     ], device=device)
+
+def get_camera_pose_cpu(position, azimuth, elevation):
+    # 将角度从度转换为弧度
+    azimuth_rad = np.radians(azimuth)
+    elevation_rad = np.radians(elevation)
+
+    # 计算旋转矩阵
+    R_azimuth = np.array([
+        [np.cos(azimuth_rad), -np.sin(azimuth_rad), 0],
+        [np.sin(azimuth_rad),  np.cos(azimuth_rad), 0],
+        [0,                    0,                   1]
+    ])
+
+    R_elevation = np.array([
+        [1, 0,                    0                   ],
+        [0, np.cos(elevation_rad), -np.sin(elevation_rad)],
+        [0, np.sin(elevation_rad),  np.cos(elevation_rad)]
+    ])
+
+    R = R_elevation @ R_azimuth
+
+    # 创建4x4变换矩阵
+    transform_matrix = np.eye(4)
+    transform_matrix[:3, :3] = R
+    transform_matrix[:3, 3] = position
+
+    return transform_matrix
 
 def rotation_matrix_y_cpu(angle):
     c, s = np.cos(angle), np.sin(angle)
@@ -69,7 +128,7 @@ def sample_image(
 
     mesh = trimesh.load(mesh_path)
     scene = pyrender.Scene.from_trimesh_scene(mesh)
-    camera = pyrender.PerspectiveCamera(yfov=fovy)
+    camera = pyrender.PerspectiveCamera(yfov=fovy * 180 / torch.pi)
     directional_light = pyrender.DirectionalLight(color=np.ones(3), intensity=3.0)
     #scene.add(directional_light, pose=light_positions)
 
@@ -87,9 +146,68 @@ def sample_image(
 
     color, depth = r.render(scene)
     img = Image.fromarray(color)
+    img.save('/nvme/yyh/threestudio/shape_sampled_img.png')
+    transform = transforms.ToTensor()
 
+    shape_img = transform(img)
+
+    shape_img = shape_img.to(device)
     r.delete() 
 
-    return img
+    return shape_img
 
+def sample_image_4x4(    
+    mesh_path, 
+    height, 
+    width, 
+    camera_positions, 
+    elevation,
+    azimuth,
+    light_positions, 
+    fovy,
+    **kwargs,
+):
+    camera_pose = get_camera_pose(camera_positions,azimuth,elevation)
+    device = camera_positions.device
+    #print(camera_pose)
+    #light_matrix = np.eye(4)
+    light_matrix = torch.eye(4, device=device)
+    light_matrix[:3, 3] = light_positions
 
+    mesh = trimesh.load(mesh_path)
+    centroid = mesh.bounding_box.centroid
+    transformation_matrix = np.eye(4)
+    transformation_matrix[:3, 3] = -centroid
+    #scene = pyrender.Scene.from_trimesh_scene(mesh)
+    scene = pyrender.Scene()
+
+    for m in mesh.geometry.values():
+        r_mesh = pyrender.Mesh.from_trimesh(m)
+        scene.add(r_mesh, pose=transformation_matrix)
+    camera = pyrender.PerspectiveCamera(yfov=fovy, aspectRatio = 1.0)
+    directional_light = pyrender.DirectionalLight(color=np.ones(3), intensity=3.0)
+    #scene.add(directional_light, pose=light_positions)
+
+    camera_pose_cpu = camera_pose.to('cpu').clone().numpy()
+    light_matrix_cpu = light_matrix.to('cpu')
+    light_matrix_cpu = light_matrix_cpu.clone().numpy()
+
+    camera_node = scene.add(camera, pose=camera_pose_cpu)
+    light_node = pyrender.Node(light=directional_light, matrix=light_matrix_cpu)
+    #scene.add_node(camera_node)
+    scene.add_node(light_node)
+    projection_matrix = camera.get_projection_matrix()
+
+    r = pyrender.OffscreenRenderer(width, height)
+
+    color, depth = r.render(scene)
+    img = Image.fromarray(color)
+    img.save('/nvme/yyh/threestudio/shape_sampled_4x4matrix_img.png')
+    transform = transforms.ToTensor()
+
+    shape_img = transform(img)
+
+    shape_img = shape_img.to(device)
+    r.delete() 
+
+    return shape_img, projection_matrix
